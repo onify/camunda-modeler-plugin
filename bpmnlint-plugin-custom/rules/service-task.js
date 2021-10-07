683379d084fa2ef4b96b4dfafc840c29dc555b34
Julian <julian.hasrama@gmail.com>
2021-10-07 20:42:46 +0200

    Service task new rules

--- bpmnlint-plugin-custom/rules/service-task.js
+++ bpmnlint-plugin-custom/rules/service-task.js
@@ -10,18 +10,47 @@ const {
  */
 module.exports = function() {
 
+  function findElement(elements, name) {
+    if (!elements || !elements.length) return false;
+    return elements.find((element) => is(element, name))
+  }
+
   function check(node, reporter) {
     if (is(node, 'bpmn:ServiceTask')) {
-      if (!node.extensionElements || !node.extensionElements.values || !node.extensionElements.values.length || !is(node.extensionElements.values[0], 'camunda:Connector')) {
+      const connector = findElement(node.extensionElements && node.extensionElements.values, 'camunda:Connector');
+
+      if (!node.extensionElements || !connector) {
         reporter.report(node.id, 'Implementation must be set');
         return reporter.report(node.id, 'Implementation only supports "Connector"');
       }
 
-      if (!node.extensionElements.values[0].connectorId) {
-        return reporter.report(node.id, `Connector Id must be set ${JSON.stringify(node)}`);
+      if (!connector.connectorId) {
+        return reporter.report(node.id, 'Connector Id must be set');
       }
-      if (node.extensionElements.values[0].connectorId !== 'httpRequest' && node.extensionElements.values[0].connectorId !== 'onifyApiRequest') {
+
+      if (connector.connectorId !== 'httpRequest' && connector.connectorId !== 'onifyApiRequest') {
         return reporter.report(node.id, 'Connector Id only supports our service tasks (eg. httpRequest, onifyApiRequest, etc)');
+      }
+
+      if (!connector.inputOutput || !connector.inputOutput.inputParameters) {
+        return reporter.report(node.id, 'Input parameters url and responseType must be set');
+      }
+
+      const url = connector.inputOutput.inputParameters.find((input) => input.name === 'url');
+      const responseType = connector.inputOutput.inputParameters.find((input) => input.name === 'responseType');
+      const method = connector.inputOutput.inputParameters.find((input) => input.name === 'method');
+      const json = connector.inputOutput.inputParameters.find((input) => input.name === 'json');
+
+      if (!url) {
+        return reporter.report(node.id, 'Input parameters url must be set');
+      }
+
+      if (!responseType) {
+        return reporter.report(node.id, 'Input parameters responseType must be set');
+      }
+
+      if (method && (method.value === 'POST' || method.value === 'PUT' || method.value === 'PATCH') && !json) {
+        return reporter.report(node.id, 'Input parameters json must be set');
       }
     }
   }
